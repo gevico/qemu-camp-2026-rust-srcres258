ef14a0c1a9d06f2f0816739de1ad1b7d0c522408
srcres258 <src.res.211@gmail.com>
2026-04-07 11:01:35 +0800

    Finish exercise try_from_into
    Signed-off-by: srcres258 <src.res.211@gmail.com>

--- exercises/conversions/try_from_into.rs
+++ exercises/conversions/try_from_into.rs
@@ -23,21 +23,15 @@ enum IntoColorError {
     IntConversion,
 }
 
-// I AM NOT DONE
-
-// Your task is to complete this implementation
-// and return an Ok result of inner type Color.
-// You need to create an implementation for a tuple of three integers,
-// an array of three integers, and a slice of integers.
-//
-// Note that the implementation for tuple and array will be checked at compile time,
-// but the slice implementation needs to check the slice length!
-// Also note that correct RGB color values must be integers in the 0..=255 range.
-
 // Tuple implementation
 impl TryFrom<(i16, i16, i16)> for Color {
     type Error = IntoColorError;
     fn try_from(tuple: (i16, i16, i16)) -> Result<Self, Self::Error> {
+        let (red, green, blue) = tuple;
+        let red = u8::try_from(red).map_err(|_| IntoColorError::IntConversion)?;
+        let green = u8::try_from(green).map_err(|_| IntoColorError::IntConversion)?;
+        let blue = u8::try_from(blue).map_err(|_| IntoColorError::IntConversion)?;
+        Ok(Color { red, green, blue })
     }
 }
 
@@ -45,6 +39,11 @@ impl TryFrom<(i16, i16, i16)> for Color {
 impl TryFrom<[i16; 3]> for Color {
     type Error = IntoColorError;
     fn try_from(arr: [i16; 3]) -> Result<Self, Self::Error> {
+        let [red, green, blue] = arr;
+        let red = u8::try_from(red).map_err(|_| IntoColorError::IntConversion)?;
+        let green = u8::try_from(green).map_err(|_| IntoColorError::IntConversion)?;
+        let blue = u8::try_from(blue).map_err(|_| IntoColorError::IntConversion)?;
+        Ok(Color { red, green, blue })
     }
 }
 
@@ -52,6 +51,13 @@ impl TryFrom<[i16; 3]> for Color {
 impl TryFrom<&[i16]> for Color {
     type Error = IntoColorError;
     fn try_from(slice: &[i16]) -> Result<Self, Self::Error> {
+        if slice.len() != 3 {
+            return Err(IntoColorError::BadLen);
+        }
+        let red = u8::try_from(slice[0]).map_err(|_| IntoColorError::IntConversion)?;
+        let green = u8::try_from(slice[1]).map_err(|_| IntoColorError::IntConversion)?;
+        let blue = u8::try_from(slice[2]).map_err(|_| IntoColorError::IntConversion)?;
+        Ok(Color { red, green, blue })
     }
 }
 
